5fce76cc71c21b7091eb18d182aea9800a82061a
habdem04 <habdem2003@gmail.com>
2025-03-30 17:37:36 +0000

    Initial Comment

---
+++ hdfp/hdfp/doctype/foreign_purchase_order/foreign_purchase_order.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, hdsd and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ForeignPurchaseOrder(Document):
+	pass
---
+++ hdfp/hdfp/doctype/foreign_purchase_status_item/test_foreign_purchase_status_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, hdsd and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestForeignPurchaseStatusItem(FrappeTestCase):
+	pass
